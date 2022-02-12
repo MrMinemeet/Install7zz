0b5113ba04f63432dc6a8f206c492f2f3d458fcf
Alex | MrMinemeet <alexander.voglsperger@gmail.com>
2022-02-12 20:17:20 +0100

    Added some output and force move

--- install.sh
+++ install.sh
@@ -5,10 +5,16 @@
 FILE="7z2107-linux-x64.tar.xz"
 
 # Download file
+echo "Downloading $FILE"
 wget -O /tmp/$FILE https://www.7-zip.org/a/$FILE
 
 # Unpack file
+echo "Unpacking file..."
 tar xf /tmp/$FILE -C /tmp
 
 # Move file to /usr/local/bin
-mv /tmp/7zz /usr/local/bin/
+echo "Installing 7zz"
+mv -f /tmp/7zz /usr/local/bin/
+
+
+echo "Installation complete!"
